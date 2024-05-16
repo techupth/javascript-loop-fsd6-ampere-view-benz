bdd3920820c4661373b87345719276f46564b703
Ampare <visarut.thk@gmail.com>
2024-05-16 13:59:04 +0700

    Assignment:JS-Loop

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let letter of companyName) {
+  reversedCompanyName = letter + reversedCompanyName;
+}
 
 console.log(reversedCompanyName);
